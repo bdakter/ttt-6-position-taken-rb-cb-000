68f17769200b02ac715310d3036797cd87fa3d5d
Boruch Z <philomath613@gmail.com>
2019-02-27 14:09:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 
 def position_taken?(board, index)
-  if board[index] == " " or board[index] == "" or board[index] == nil
+  # if board[index] == " " or board[index] == "" or board[index] == nil
+  if board[index] in [" ", "", nil]
     false
   else
     true
